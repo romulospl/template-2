8a5cf3abd148b7add91f9ba476e89e6e44d97fc7
romulospl <romulopereiralopes@gmail.com>
2023-03-04 19:17:24 -0300

    pinia instalado e testado

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,27 @@
+<script setup>
+import { mapActions, storeToRefs } from 'pinia';
+import { useMainStore } from '../store/main';
+
+const mainStore = useMainStore()
+
+const { counter } = storeToRefs(mainStore)
+const { increment } = mapActions(useMainStore, ["increment"])
+</script>
+
 <template>
     <div>
         <h1>Home</h1>
     </div>
+    <div class="w-fit mx-auto mt-8">
+      <div>
+        <span>Pinia example: </span>
+      </div>
+      <button class="main-button" type="button" @click="increment">count is {{ counter }}</button>
+    </div>
 </template>
+
+<style scoped>
+.main-button{
+    border: 1px solid #e7e4e4;
+}
+</style>
