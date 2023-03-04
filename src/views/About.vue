dd650e0cc8ea19b4236a096a3824b2392792a09e
romulospl <romulopereiralopes@gmail.com>
2023-03-04 19:20:15 -0300

    axios instaled

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import axios from 'axios'
+
+const text = ref('')
+
+onMounted(() => {
+  axios.get('https://api.adviceslip.com/advice')
+    .then(function (response) {
+      text.value = response.data.slip.advice;
+    })
+})
+</script>
+
 <template>
     <div>
         <h2>About</h2>
+    </div>
+    <div>
+      <span>Example axios, advice: </span>
+      <span>{{ text }}</span>
     </div>
 </template>
